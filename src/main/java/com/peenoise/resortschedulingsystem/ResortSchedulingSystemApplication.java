f897c2d4652219bc8fc1419af91a361313ff8164
tope31 <111934264+tope31@users.noreply.github.com>
2023-05-30 15:21:50 +0800

    added swagger

--- src/main/java/com/peenoise/resortschedulingsystem/ResortSchedulingSystemApplication.java
+++ src/main/java/com/peenoise/resortschedulingsystem/ResortSchedulingSystemApplication.java
@@ -1,13 +1,26 @@
 package com.peenoise.resortschedulingsystem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Resort Scheduling System API Documentation",
+                description = "Peenoise Resort Scheduling System",
+                version = "v1.0"
+        )
+
+)
 public class ResortSchedulingSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResortSchedulingSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ResortSchedulingSystemApplication.class, args);
+    }
+
 
 }
--- src/test/java/com/peenoise/resortschedulingsystem/ResortSchedulingSystemApplicationTests.java
+++ src/test/java/com/peenoise/resortschedulingsystem/ResortSchedulingSystemApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ResortSchedulingSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
